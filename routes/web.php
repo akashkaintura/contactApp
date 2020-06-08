c78460d4d89076304270f02982ae6c416409d4bd
Akash Kaintura <vikaskaintura63@gmail.com>
2020-06-08 13:20:47 +0530

    auth

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,4 @@ Route::get('/contacts/{id}/edit', 'ContactController@edit')->name('contacts.edit
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('home');
